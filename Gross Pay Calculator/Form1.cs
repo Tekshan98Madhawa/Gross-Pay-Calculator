f272c79001a679b4f27339625bf270888bb0a1df
Tekshan Madhawa <72126578+Tekshan98Madhawa@users.noreply.github.com>
2023-04-05 12:29:12 +0530

    Add files via upload

---
+++ Gross Pay Calculator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gross_Pay_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double hoursWorked = double.Parse(textBox1.Text);
+            double totalHours = double.Parse(textBox2.Text);
+            double grossPay = hoursWorked * totalHours;
+            textBox3.Text = grossPay.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
